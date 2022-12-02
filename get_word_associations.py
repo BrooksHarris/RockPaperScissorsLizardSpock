565db4b31ee8c0dcf5f0405a51e0e72edf1a5bd2
Raghav-Srinivasan0 <raghavsrinivasan0@gmail.com>
2022-12-02 09:11:17 -0500

    quality of life changes to get_word_associations

--- get_word_associations.py
+++ get_word_associations.py
@@ -1,6 +1,11 @@
 #pip install --user -U nltk
-#import nltk
-#nltk.download()
-from nltk.corpus import wordnet as wn
-all_nouns = [word for synset in wn.all_synsets('n') for word in synset.lemma_names()]
-# all_nouns is a list of length 146,347
+
+#set variable below to true to initiate setup mode
+setup = False
+if setup:
+    import nltk
+    nltk.download()
+else:
+    from nltk.corpus import wordnet as wn
+    all_nouns = [word for synset in wn.all_synsets('n') for word in synset.lemma_names()]
+    # all_nouns is a list of length 146,347
