3c9dce3e6a68a58b5d9828fb08c2368aa8e9ec1c
Raghav-Srinivasan0 <raghavsrinivasan0@gmail.com>
2022-12-07 12:49:46 -0500

    openai game generation code v1

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ wow
 word2vec from Google .bin file: https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit?resourcekey=0-wjGZdNAUop6WykTtMip30g
 
 in setup mode for get_word_associations.py, select all packages and download it to C:/
+
+use openai to generate the rock paper scissors pairings
---
+++ openai-game-generation.py
@@ -0,0 +1,53 @@
+import os
+import openai
+
+names = ["rock","paper","scissors","lizard","spock"]
+b = ["rock","scissors","paper","spock","paper","spock","lizard","lizard","rock","scissors"]
+w = ["scissors","paper","rock","rock","spock","scissors","spock","paper","lizard","lizard"]
+import os
+import openai
+
+openai.api_key = "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" #os.getenv("OPENAI_API_KEY")
+
+NUMBER_OF_ELEMENTS = 10
+
+for i in range(NUMBER_OF_ELEMENTS):
+    #, and generate a list of {} uses
+    prompt = """Write a new type of element for the game {}.
+        """.format(", ".join(names)
+        #,str(len(names))
+        )
+    prompt_append = ""
+    for j in names:
+        prompt_append = prompt_append + "\nElement: " + j + "\nUses: "
+        for k in range(len(b)):
+            if b[k] == j:
+                prompt_append = prompt_append + "Wins against " + w[k] + ", "
+            if w[k] == j:
+                prompt_append = prompt_append + "Loses to " + b[k] + ", "
+    prompt = prompt + prompt_append
+    print(prompt)
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    result = response["choices"][0]["text"]
+    print(result)
+    names.append(result[int(result.find("Element: ")) + 9:int(result.find("\nUses: "))].lower())
+    find_within = result[result.find("Uses: ") + 6:]
+    for element in find_within.split(", "):
+        element = element.replace(".","").replace(",","").replace(";","").replace("/","").replace("\\","")
+        if element.find("Wins against ") != -1:
+            b.append(names[-1])
+            w.append(element[element.find("Wins against ")+13:])
+        if element.find("Loses to ") != -1:
+            w.append(names[-1])
+            b.append(element[element.find("Loses to ")+9:])
+    #print(names)
+    #print(b)
+    #print(w)
